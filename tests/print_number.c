f8a5e802717c3baccbb78d9562bd0a82b8965066
Panda <linimbus@126.com>
2017-11-11 12:00:35 +0800

    update test code

--- tests/print_number.c
+++ tests/print_number.c
@@ -24,7 +24,7 @@
 #include "unity/src/unity.h"
 #include "common.h"
 
-static void assert_print_number(const char *expected, double input)
+static void assert_print_number(const char *expected, long double input)
 {
     unsigned char printed[1024];
     cJSON item[1];
@@ -64,20 +64,20 @@ static void print_number_should_print_positive_integers(void)
 static void print_number_should_print_positive_reals(void)
 {
     assert_print_number("0.123", 0.123);
-    assert_print_number("1e-09", 10e-10);
+    assert_print_number("1e-10", 10e-11);
     assert_print_number("1000000000000", 10e11);
-    assert_print_number("1.23e+129", 123e+127);
-    assert_print_number("1.23e-126", 123e-128);
-    assert_print_number("3.1415926535897931", 3.1415926535897931);
+    assert_print_number("1.23e+129", 1.23e+129);
+    assert_print_number("1.23e-126", 1.23e-126);
+    assert_print_number("3.141592653589793", 3.141592653589793);
 }
 
 static void print_number_should_print_negative_reals(void)
 {
     assert_print_number("-0.0123", -0.0123);
-    assert_print_number("-1e-09", -10e-10);
+    assert_print_number("-1e-10", -10e-11);
     assert_print_number("-1e+21", -10e20);
-    assert_print_number("-1.23e+129", -123e+127);
-    assert_print_number("-1.23e-126", -123e-128);
+    assert_print_number("-1.23e+129", -1.23e+129);
+    assert_print_number("-1.23e-126", -1.23e-126);
 }
 
 static void print_number_should_print_non_number(void)
